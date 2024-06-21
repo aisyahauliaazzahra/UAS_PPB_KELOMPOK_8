815bc3c62e9e09c3e0af2778e59460b565bc1694
ASUS <aisyahaap1616@gmail.com>
2024-06-21 22:48:57 +0700

    commit3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.firebase:firebase-database:21.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -112,7 +111,6 @@ dependencies {
     implementation ("com.google.firebase:firebase-storage:20.0.1") // Example for Firebase Storage
 
     implementation ("de.hdodenhof:circleimageview:3.1.0")
-    implementation ("com.google.firebase:firebase-storage-ktx:20.1.0")
 
     androidTestImplementation ("androidx.test.ext:junit:1.1.4")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.0")
