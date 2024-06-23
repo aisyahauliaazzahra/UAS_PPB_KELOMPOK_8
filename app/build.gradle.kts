5d8fb9a13251d17928d3c721128d28d9dcefe7bf
aisyahauliaaa <156505246+aisyahauliaaa@users.noreply.github.com>
2024-06-23 19:17:06 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,24 +6,20 @@ plugins {
     id("kotlin-kapt")
     id("com.google.gms.google-services")
 }
-
 android {
     namespace = "com.example.ppb_uas"
     compileSdk = 34
-
     defaultConfig {
         applicationId = "com.example.ppb_uas"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -42,8 +38,8 @@ android {
     }
     buildFeatures {
         compose = true
-        dataBinding = true
-        mlModelBinding = true
+        dataBinding=true
+        mlModelBinding=true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -54,7 +50,6 @@ android {
         }
     }
 }
-
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
@@ -67,52 +62,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.room:room-runtime:2.4.2")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.room:room-ktx:2.4.2")
-    implementation("com.stripe:stripe-android:20.0.0")
+	@@ -112,7 +111,6 @@ dependencies {
+    implementation ("com.google.firebase:firebase-storage:20.0.1") // Example for Firebase Storage
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("io.reactivex.rxjava3:rxjava:3.1.3")
-    implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
-    implementation("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 
-    implementation("org.tensorflow:tensorflow-lite:2.10.0")
-    implementation("org.tensorflow:tensorflow-lite-support:0.4.3")
-    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0-rc2")
-    implementation("org.tensorflow:tensorflow-lite-gpu:2.10.0")
-
-    implementation("com.google.code.gson:gson:2.9.0")
-
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
-
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
-
-    implementation("com.airbnb.android:lottie:4.2.2")
-
-    // Firebase BoM, to align versions
-    implementation(platform("com.google.firebase:firebase-bom:31.5.0"))
-
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore-ktx")
-    implementation("com.google.firebase:firebase-database")
-    implementation("com.google.firebase:firebase-storage")
-
-    implementation("de.hdodenhof:circleimageview:3.1.0")
-
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-}
+    androidTestImplementation ("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
